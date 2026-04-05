1da2ad20e9ef908a29ac4391337963c5761f4870
kristian-tichota <kristian.tichota@proton.me>
2026-04-05 14:15:22 +0200

    Fix critical typo

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
         self.model_name = data.get("model_name", "base")
         self.silence_seconds = data.get("silence_seconds", 1.0)
         self.max_chunk_seconds = data.get("max_chunk_seconds", 30.0)
-        self.energy_threshold = data.get("energy_treshold", 0.01)
+        self.energy_threshold = data.get("energy_threshold", 0.01)
         self.pre_roll_seconds = data.get("pre_roll_seconds", 0.25)
         self.lang = data.get("lang", "en")
         self.input_delay = data.get("input_delay", 0.01)
